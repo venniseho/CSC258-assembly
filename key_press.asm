4264be4175d5c348c71bd3e469ecf4fe7eaee800
Vennise Ho <venniseho14@gmail.com>
2024-11-16 23:58:22 -0500

    Completed check_key_press function

---
+++ key_press.asm
@@ -0,0 +1,64 @@
+############################## Check Key Press ###############################
+######################## Bitmap Display Configuration ########################
+# - Unit width in pixels:       8
+# - Unit height in pixels:      8
+# - Display width in pixels:    256
+# - Display height in pixels:   256
+# - Base Address for Display:   0x10008000 ($gp)
+##############################################################################
+.data
+display_address:     .word 0x10008000
+keyboard_address:    .word 0xffff0000
+
+red:                .word 0xff0000
+yellow:             .word 0xffff00
+blue:               .word 0x00ffff
+white:              .word 0xffffff
+black:              .word 0x000000
+
+a:          .word 0x61
+d:          .word 0x64
+w:          .word 0x77
+s:          .word 0x73
+q:          .word 0x71
+n:          .word 0x6e
+
+left_init_position:     .word 2236
+
+game_array:         .word 0:128
+
+.text
+
+# START CHECK_KEY_PRESS
+# function that checks if a key was pressed
+#       if a key was pressed and it is valid (w, a, s, d, q), return it's corresponding letter.
+#       if a key was not pressed or if it is not a valid letter, return n (for null)
+
+# returns: v0 (the ASCII code for a letter - w, a, s, d, q, n)
+# registers: t7 (ASCII key value of key pressed), t8 (value at keyboard_address --> 0 or 1), t9 (keyboard_address)
+check_key_press:
+    lw $t9, keyboard_address        # $t9 = base keyboard address
+    lw $t8, 0($t9)                  # $t8 = value at keyboard address
+    beq $t8, 1, keyboard_input      # if $t9 == 1: key was pressed (ASCII key value found in next value in memory)
+    
+    
+# IF A KEY WAS PRESSED, GET ITS ASCII VALUE
+keyboard_input:
+    lw $t7, 4($t9)                  # $t7 = ASCII key value 
+    
+    beq $t7, 0x61, valid_key        # a was pressed
+    beq $t7, 0x64, valid_key        # d was pressed
+    beq $t7, 0x77, valid_key        # w was pressed
+    beq $t7, 0x73, valid_key        # s was pressed
+    beq $t7, 0x71, respond_to_q     # q was pressed
+    
+    lw $v0, n                       # if none of the valid keys (w, a, s, d, q) were pressed, return n (v0 = ASCII code of n)
+    jr $ra                          
+
+    valid_key:
+    add $v0, $t7, $zero             # return the ASCII key value of the valid key
+    jr $ra
+    
+    respond_to_q:
+	li $v0, 10                      # quit gracefully
+	syscall
