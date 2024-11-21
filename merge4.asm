e313d38df262f8499c796e709943e8b9e11b110d
Vennise Ho <venniseho14@gmail.com>
2024-11-21 00:27:16 -0500

    commit for comparison

--- merge4.asm
+++ merge4.asm
@@ -60,7 +60,7 @@ colour_2:               .word 0             # current colour of half 2
 
 # START OF MERGE_ROW
 # returns: v0 (game_array offset of the start of 4 same colours in a row or 0 if there are none)
-# registers: t0 (game_array pointer), t1 (y/row count), t2 (loop unit count), t3 (max units in a row count; from call the check_merge_row), 
+# registers: t0 (game_array pointer), t3 (y/row count), t4 (loop unit count), t6 (max units in a row count; from call the check_merge_row), 
 #            t5 (game_array address), t9 (the colour black)
 merge_row:
     subi $sp, $sp, 4            # Decrease stack pointer (make space for a word)
@@ -69,33 +69,33 @@ merge_row:
     lw $t9, black
     
     # loop that checks each row to see if we there are at least 4 in a row, and merges if so. (starts from bottom)
-    addi $t1, $zero, 15         # initialise the row count (starts from bottom, last row = 15)
+    addi $t3, $zero, 15         # initialise the row count (starts from bottom, last row = 15)
     
     merge_row_loop:
         # call to check_merge_row of the current row to find out if a merge is needed. 
-        add $a0, $t1, $zero                     # arg to check_merge_row; current y/row value
+        add $a0, $t3, $zero                     # arg to check_merge_row; current y/row value
         jal check_merge_row
         
         add $t0, $t5, $v0                       # set the game_array pointer to the base address + returned offset of the last of the same colour units (-1 = no units to merge)
-        add $t3, $zero, $v1                     # t3 = v1 = the number of units to merge (0 or a value >=4) 
+        add $t6, $zero, $v1                     # t6 = v1 = the number of units to merge (0 or a value >=4) 
         
-        beq $t3, $zero, decrement_merge_row_loop        # if the number of units to merge is zero, go on to the next loop (check the next row)
+        beq $t6, $zero, decrement_merge_row_loop        # if the number of units to merge is zero, go on to the next loop (check the next row)
         
         # otherwise, the number of units to merge >= 4
         # loop that changes all units in a row of the same colour to black
-        add $t2, $zero, $zero                   # set the loop unit count to 0
+        add $t4, $zero, $zero                   # set the loop unit count to 0
         
         merge_units_loop:
             sw $t9, 0($t0)                          # sets the value at game_array pointer to black
             addi $t0, $t0, -4                       # decreases the game_array pointer by 4 because it was initialised at the last of the same colour units
-            addi $t2, $t2, 1                        # increment the loop counter by 1
-            bne $t2, $t3, merge_units_loop          # loops until the loop counter = the number of units to merge
+            addi $t4, $t4, 1                        # increment the loop counter by 1
+            bne $t4, $t6, merge_units_loop          # loops until the loop counter = the number of units to merge
         ############## CALL MERGE ALL CAPSULES DOWN #############################################################################################
         j merge_row_loop                            # jump to start of merge_row_loop to check the current row again for any more same colours
         
         decrement_merge_row_loop:
-            addi $t1, $t1, -1                       # decrease the row number by 1
-            blt $t1, 0, merge_row_loop              # loops until it hits the top row of the game
+            addi $t3, $t3, -1                       # decrease the row number by 1
+            bge $t3, 0, merge_row_loop              # loops until it hits the top row of the game
 
 exit_merge_row:
     lw $ra, 0($sp)           # Load the saved value of $ra from the stack
@@ -112,40 +112,47 @@ check_merge_row:
     sw $ra, 0($sp)              # Store the value of $ra at the top of the stack
 
     lw $t9, black               # load the colour black into t9
-    lw $t5, game_array          # load the game array base address into t5
+    la $t5, game_array          # load the game array base address into t5
     
     # need to add 32 y times
     add $t0, $t5, $zero         # initialise the game pointer
     add $t2, $zero, $zero       # initialise the loop counter
+    beq $t2, $a0, skip_loop_to_y
     
     # loop that adds 32 (8 units per row x 4 bits per unit) y times to set game_array offset
     loop_to_y:
     addi $t0, $t0, 32           # add 32 each loop (bits per row)
     addi $t2, $t2, 1            # increment the loop counter
-    bne $t2, $a0, loop_to_y     # loop until loop counter = y rows
+    ble $t2, $a0, loop_to_y     # loop until loop counter = y rows
     
+    skip_loop_to_y:
     # loop that checks the row for 4 consecutive values of the same colour
-    add $t1, $zero, $zero       # initialise the colour count
+    addi $t1, $zero, 1          # initialise the colour count
     lw $t7, black               # initialise the current colour
     add $t2, $zero, $zero       # reset the loop counter
     
     check_merge_row_loop:
         lw $t8, 0($t0)              # load the colour at the game array index into t8
         
-        beq $t8, $t9, increment_check_merge_row_loop      # if t8 is black, go to next loop
-        beq $t7, $t8, check_merge_row_loop_equals   # if the current colour and next colour are equal, jump to that condition
+        beq $t7, $t8, check_merge_row_loop_equals       # if the current colour and next colour are equal, jump to that condition
         
         # otherwise, we know that the next colour is different from the current colour (and is not black)
         bge $t1, 4, return_merge_row_positive       # if the colour count >= 4, jump to return
         
         add $t7, $t8, $zero                         # set current colour = next colour
-        add $t1, $zero, $zero                       # reset the colour count
+        addi $t1, $zero, 1                          # reset the colour count
         j increment_check_merge_row_loop
         
         # we know that the current colour and next colour are equal. increment the colour count by 1 and check for 4 in a row.
         check_merge_row_loop_equals:
+            beq $t8, $t9, check_merge_row_loop_black        # if t8 is black, go to next loop
             addi $t1, $t1, 1                            # increment the current colour count by 1
             j increment_check_merge_row_loop
+        
+        # we know that the current colour is black
+        check_merge_row_loop_black:
+            addi $t1, $zero, 1                              # reset the colour count
+            j increment_check_merge_row_loop                # next loop
         
         increment_check_merge_row_loop:
             addi $t2, $t2, 1                            # increase the loop counter by 1
@@ -162,9 +169,9 @@ check_merge_row:
     
     # there are four consecutive same colours
     return_merge_row_positive:
-    add $t0, $t0, $t5                           # the game_array offset (last value)
+    sub $t0, $t0, $t5                           # the game_array offset (last value)
     add $v0, $t0, $zero                         # returns the game_array offset
-    add $v0, $t1, $zero                         # returns the number of units that are the same colour
+    add $v1, $t1, $zero                         # returns the number of units that are the same colour
     j exit_check_merge_row
     
     exit_check_merge_row:
@@ -172,8 +179,3 @@ check_merge_row:
     addi $sp, $sp, 4         # Increase the stack pointer (free up space)
     jr $ra
 # END OF CHECK_MERGE_ROW
-
-
-
-
-
